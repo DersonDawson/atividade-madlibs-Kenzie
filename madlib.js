27d299a47a78228fc9556304577ea847165157fd
DersonDawson <andersonsmurf@gmail.com>
2021-12-05 23:50:29 -0300

    Projeto MADLIB

---
+++ madlib.js
@@ -0,0 +1,15 @@
+let place = prompt("Escreva o nome de um lugar: ");
+let hero = prompt("Escreva o nome de um héroi da Marvel: ");
+let vilain = prompt("Escreva o nome de um vilão da Marvel: ");
+let me = prompt("Escreva o seu nome: ");
+       
+document.write("Aqui está uma história personalizada:<br>");  
+        
+document.write("Eu "+ me + " Estive em " + place + ", quando vi o " + hero + 
+", lutando em um embate mortal contra  " + vilain + 
+" e mal pude acreditar que era real. O " + hero + " quase perdia." + 
+"até que o Tobey Maguire, apareceu e salvou o " 
++ hero + " acabando com todas as chances do " + vilain + ".");  
+
+
+ 
